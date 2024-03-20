185e49a264a68039362953beb020a7b560c1e186
NaveenNaviii <navee9900@gmail.com>
2024-03-20 16:55:03 +0530

    style binding

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  //Property biinding
+  val='Navi Bhai'
+
+  //style binding
+  color:boolean=false
+  c1='red'
+  c2='green'
+
+
+}
