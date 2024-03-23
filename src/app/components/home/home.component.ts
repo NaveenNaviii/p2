7165ae9ef986941a738101b07e4e13438fe2e64a
NaveenNaviii <navee9900@gmail.com>
2024-03-23 13:40:45 +0530

    services added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  
+  constructor(private user:UsersService){}
 
+  result:any
 
+  ngOnInit(): void {
+    this.user.getData().subscribe((data)=>{
+      this.result=data;
+    })
+  }
 }
